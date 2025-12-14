0e15c461f0271ab6613c2d5e3e2b4e5feb334c78
GabiZar <GabiZar@proton.me>
2025-12-14 19:33:08 +0100

    initial commit

---
+++ scripts/manager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@onready var root = Engine.get_main_loop().root
+@onready var player = root.get_node("Main/Player")
+@onready var terrain = root.get_node("Main/PlaceHolderTerrain")
+
+var is_dash_unlocked := false
+var is_double_jump_unlocked := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func item_grabed(item_name):
+	print(item_name + " picked up")
+	if item_name == "dash":
+		is_dash_unlocked = true
+	elif item_name == "double_jump":
+		is_double_jump_unlocked = true
+	else:
+		print("unknown item")
---
+++ scripts/place_holder_terrain.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$CollisionPolygon2D.polygon = $Polygon2D.polygon
