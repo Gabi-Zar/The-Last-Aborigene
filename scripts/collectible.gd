fb51a5d49024a31ad93ae65edc43a2dfad4d98f6
GabiZar <GabiZar@proton.me>
2025-12-17 09:59:33 +0100

    added particle effect to the collectable

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -5,15 +5,24 @@ extends RigidBody2D
 @onready var collectible_area = $GrabArea2D
 @onready var collectible_floor_collision = $FloorCollisionShape2D
 @onready var collectible_floor_detector = $FloorDetectorRayCast2D
+@onready var collectible_sprite = $Sprite2D
 @onready var collectible_sprite_animation = $Sprite2D/AnimationPlayer
 @onready var collectible_label = $Label
 @onready var collectible_label_animation = $Label/AnimationPlayer
+@onready var collectible_particle = $CPUParticles2D
 @onready var player = Manager.player
+
+var death_timer := Timer.new()
+var is_grabed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	collectible_sprite_animation.play("collectible_animation")
 	apply_impulse(Vector2(randf_range(-200,200), -500))
+	
+	death_timer.wait_time = 0.5
+	death_timer.one_shot = true
+	add_child(death_timer)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,6 +31,11 @@ func _process(_delta: float) -> void:
 		collectible_label_animation.play("text_appear")
 		if Input.is_action_just_pressed("Grab"):
 			Manager.item_grabed(collectible_name)
+			collectible_sprite.hide()
+			is_grabed = true
+			collectible_particle.restart()
+			death_timer.start()
+		if death_timer.is_stopped() and is_grabed == true:
 			queue_free()
 	else:
 		collectible_label_animation.play("text_disappear")
