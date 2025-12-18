12cba51c7e2929a252538653388b388462d5309d
GabiZar <GabiZar@proton.me>
2025-12-18 18:43:37 +0100

    Added the boomrang WIP and UI assets

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,20 +4,30 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -200.0
 const JUMP_MAXIMUM_VELOCITY = -600.0
-const DASH_VELOCITY = 2000
+const DASH_VELOCITY = 1500
 
 var latest_direction : float 
-var dash_cooldown := Timer.new()
 var double_jump_cooldown := false
 var jump_limit = 0.0
 var is_jumping := false
 
-@onready var Sprite = $AnimatedSprite2D
+var dash_cooldown := Timer.new()
+var boomrang_cooldown := Timer.new()
+
+@export var boomrang: PackedScene
+var boomrang_instance: Node
+
+@onready var sprite = $AnimatedSprite2D
+@onready var boomrang_sprite = $BoomrangSprite2D
 
 func _ready() -> void:
 	dash_cooldown.wait_time = 1
 	dash_cooldown.one_shot = true
 	add_child(dash_cooldown)
+	
+	boomrang_cooldown.wait_time = 0.5
+	boomrang_cooldown.one_shot = true
+	add_child(boomrang_cooldown)
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -56,9 +66,13 @@ func _physics_process(delta: float) -> void:
 		latest_direction = direction
 		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED)
 		if direction == 1:
-			Sprite.set_animation("right")
+			sprite.set_animation("right")
+			boomrang_sprite.position.x = -50.0
+			boomrang_sprite.rotation_degrees = 270.0
 		else:
-			Sprite.set_animation("left")
+			sprite.set_animation("left")
+			boomrang_sprite.position.x = 50.0
+			boomrang_sprite.rotation_degrees = 0.0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
@@ -67,5 +81,21 @@ func _physics_process(delta: float) -> void:
 			if dash_cooldown.is_stopped():
 				velocity.x += latest_direction * DASH_VELOCITY
 				dash_cooldown.start()
-
+	
 	move_and_slide()
+	
+	############# Attaque #############
+	if boomrang_cooldown.is_stopped():
+		boomrang_sprite.show()
+		if Input.is_action_just_pressed("Attack"):
+			boomrang_instance = boomrang.instantiate()
+			boomrang_instance.position = position + boomrang_sprite.position
+			boomrang_instance.rotation = boomrang_sprite.rotation
+			if boomrang_instance.rotation == 0.0:
+				boomrang_instance.angular_velocity = deg_to_rad(-720)
+			else:
+				boomrang_instance.angular_velocity = deg_to_rad(720)
+			Manager.main.add_child(boomrang_instance)
+			
+			boomrang_sprite.hide()
+			boomrang_cooldown.start()
